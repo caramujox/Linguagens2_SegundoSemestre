1be5ece1751c6a97a4a30eba5e54a8ab6a1946d3
Caio de Araujo Morais <caioa.morais@gmail.com>
2019-09-16 22:25:02 -0300

    Indo pra segurança GodHelpMe

--- FlaskAlura/app.py
+++ FlaskAlura/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -7,9 +7,32 @@ app = Flask(__name__)
 # def hello():
 #     return "HelloWorld!"
 
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
 
-@app.route('/jogoteca')
-def jogoteca():
-    return render_template('lista.html')
+jogo1 = Jogo('Super Mario', 'Acao', 'SNES')
+jogo2 = Jogo('Pokemon Gold', 'RPG', 'GBA')
+lista = [jogo1, jogo2]       
 
-app.run()
+
+@app.route('/')
+def index():
+    return render_template('lista.html', titulo = 'Jogos', jogos = lista)
+
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html', titulo = 'Cadastre o seu novo Jogo')
+
+@app.route('/criar_jogo', methods = ['POST',])
+def criar_jogo():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return redirect('/')
+
+app.run(debug = True)
